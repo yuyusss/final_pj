5b56d972e56fcb858460f97c51661f3a461fb2d8
yonghwi97 <55828458+yonghwi97@users.noreply.github.com>
2022-02-22 09:29:22 +0900

    2022.02.22

--- src/main/java/com/project/board/controller/IndexController.java
+++ src/main/java/com/project/board/controller/IndexController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Service
+@Service 
 @Controller
 public class IndexController {
   
