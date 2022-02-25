f519eef8c121780c9d550ab2a1b0f7e4aafd9311
yonghwi97 <55828458+yonghwi97@users.noreply.github.com>
2022-02-25 11:42:26 +0900

    2022-02-25

--- src/main/java/com/project/board/controller/IndexController.java
+++ src/main/java/com/project/board/controller/IndexController.java
@@ -40,18 +40,14 @@ public class IndexController {
     private WeatherService weatherService;
 
 
-	 @RequestMapping(value="/", method=RequestMethod.GET) public String
-	 index(Model model) { ArrayList<MediaVO> mediaList = service.listAllMedia();
+	 @RequestMapping(value="/", method=RequestMethod.GET)
+	 public String index(Model model) {
+	 ArrayList<MediaVO> mediaList = service.listAllMedia();
 	 model.addAttribute("mediaList", mediaList);
 	  
-	 return "index2"; }
+	 return "index2";
+	 }
 
-
-//        ArrayList<CalendarVO> calenList = calendarService.calenList();;
-//        model.addAttribute("calenList", calenList);
-
-        return "index2";
-    }
 
     @RequestMapping("/vote")
     public String vote(Model model){
@@ -60,6 +56,7 @@ public class IndexController {
 
         return "vote";
     }
+}
 
  /*   @RequestMapping("/voteView")
     @ResponseBody
