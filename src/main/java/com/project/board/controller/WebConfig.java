bfb499583926c13e7fcd711a6700c4f8b1cce6f2
yuyusss <gch04713@naver.com>
2022-03-10 23:37:17 +0900

    3/1033

--- src/main/java/com/project/board/controller/WebConfig.java
+++ src/main/java/com/project/board/controller/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/images/**")
+		registry.addResourceHandler("/images1/**")
 		.addResourceLocations("file:///upload/");
 
 		/*registry.addResourceHandler("/images/**")
