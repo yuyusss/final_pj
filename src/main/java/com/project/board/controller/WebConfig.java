2d641844dd5b95a0487bcec24895abea8de73fde
yuyusss <97606045+yuyusss@users.noreply.github.com>
2022-03-10 22:50:05 +0900

    Update WebConfig.java

--- src/main/java/com/project/board/controller/WebConfig.java
+++ src/main/java/com/project/board/controller/WebConfig.java
@@ -9,11 +9,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		/*registry.addResourceHandler("/images/**")
-		.addResourceLocations("file:///upload/");*/
-
 		registry.addResourceHandler("/images/**")
-				.addResourceLocations("file:///Users/hyeonji/full_stack_study/upload/");
+		.addResourceLocations("file:///upload/");
+
+		/*registry.addResourceHandler("/images/**")
+				.addResourceLocations("file:///Users/hyeonji/full_stack_study/upload/");*/
 		/*
 		 * registry.addResourceHandler("/voice/**")
 		 * .addResourceLocations("file:///C:/upload/");
